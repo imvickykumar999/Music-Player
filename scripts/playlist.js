955fabe556361eb6bf0f8979044e4cb80ea714ce
Vicky Kumar <imvickykumar999@gmail.com>
2020-08-23 22:30:19 +0530

    Update playlist.js

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -3,5 +3,11 @@ trackList = [
     name: "Sare-Jahan-Se-Acha",
     artist:"TeknoAxe",
     src: "music/Sare-Jahan-Se-Acha.mp3"
+  },
+  
+  {
+    name: "Sare-Jahan-Se-Acha",
+    artist:"TeknoAxe",
+    src: "music/Sare-Jahan-Se-Acha.mp3"
   }
 ];
