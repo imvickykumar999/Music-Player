8dfbe072f8ff4f68dc7e8fbd6a551eda5d27e25b
Vicky Kumar <imvickykumar999@gmail.com>
2020-08-24 09:47:53 +0530

    Update playlist.js

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -10,4 +10,35 @@ trackList = [
     artist:"Sare-Jahan-Se-Acha",
     src: "music/Sare-Jahan-Se-Acha.mp3"
   }
+  ,
+  
+  {
+    name: "Aj Kal Ve",
+    artist:"Aj Kal Ve",
+    src: "music/Aj Kal Ve.mp3"
+  },
+  
+  {
+    name: "Pachtaoge Female Version - Asees Kaur 128 Kbps Mp3 Song MP3 Songs Pop New 2020 Bestwap",
+    artist:"Pachtaoge Female Version - Asees Kaur 128 Kbps Mp3 Song MP3 Songs Pop New 2020 Bestwap",
+    src: "music/Pachtaoge Female Version - Asees Kaur 128 Kbps Mp3 Song MP3 Songs Pop New 2020 Bestwap.mp3"
+  },
+  
+  {
+    name: "Sub Urban - Cradles",
+    artist:"Sub Urban - Cradles",
+    src: "music/Sub Urban - Cradles.mp3"
+  },
+  
+  {
+    name: "WhatsApp Audio 2020-08-24 at 02.14.23",
+    artist:"WhatsApp Audio 2020-08-24 at 02.14.23",
+    src: "music/WhatsApp Audio 2020-08-24 at 02.14.23.mp3"
+  },
+  
+  {
+    name: "Gajban - Sapna Choudhary",
+    artist:"Gajban - Sapna Choudhary",
+    src: "music/Gajban - Sapna Choudhary.mp3"
+  }
 ];
